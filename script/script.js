8574ed6fb2dbff12c4cdf573bcd84b9d266eccf5
bieucbt <bieucbt@gmail.com>
2022-03-11 07:23:01 +0700

    upload

--- script/script.js
+++ script/script.js
@@ -7,8 +7,18 @@ function handleSlide(btn_left, btn_right, items, slider, auto) {
     let index = 0; // dùng để tính vị trí 
     let sum = 0; // lấy tổng của item rồi cách ra như kiểu trượt
     const length = items.length;
-    const width = items[0].offsetWidth
+    let width 
+
+    // cập nhật lại kích thước slide khi chạy
+    setInterval(() => {
+        width = items[0].offsetWidth
+    },100) 
+
+
+    // xử lý khi nhấn bên trái chuyển slide
     btn_left.addEventListener('click', () => {
+        auto = false
+
         if(index <= 0) {
             index = (length - 1)
             sum -= (width * (length - 1))
@@ -20,7 +30,26 @@ function handleSlide(btn_left, btn_right, items, slider, auto) {
         }
     })
 
-    if(auto === true){
+    // xử lý khi nhấn bên phải chuyển slide
+    btn_right.addEventListener('click', () => {
+        auto = false
+        if(index >= length - 1) {
+            index = 0
+            sum = 0
+            slider.style = `transform: translateX(${sum}px)`
+        }else {
+            index ++
+            sum -= width
+            slider.style = `transform: translateX(${sum}px)`
+        }
+        
+    })
+    // làm chậm slide auto khi kích chuyển slide bằng tay
+    setInterval(() => {auto = true}, 5000)
+
+
+    // slide chạy tự động
+    if(auto == true){
         setInterval(() => {
             if(index >= length - 1) {
                 index = 0
@@ -36,18 +65,7 @@ function handleSlide(btn_left, btn_right, items, slider, auto) {
         }, 4000)
     }
 
-    btn_right.addEventListener('click', () => {
-       
-        if(index >= length - 1) {
-            index = 0
-            sum = 0
-            slider.style = `transform: translateX(${sum}px)`
-        }else {
-            index ++
-            sum -= width
-            slider.style = `transform: translateX(${sum}px)`
-        }
-    })
+    
 }
 
 

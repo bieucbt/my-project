3e990ec298f89ddcc00a2295b47bca8b3929c666
bieucbt <bieucbt@gmail.com>
2022-04-09 15:05:48 +0700

    upload

--- script/swiper.js
+++ script/swiper.js
@@ -16,14 +16,3 @@ var swiper = new Swiper(".banner", {
   },
 });
 
-
-
-  // var swiper = new Swiper(".items", {
-  //   slidesPerView: 3,
-  //   spaceBetween: 30,
-  //   freeMode: true,
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-  // });
